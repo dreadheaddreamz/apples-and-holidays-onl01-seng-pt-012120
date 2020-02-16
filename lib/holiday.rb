90cfd634cca31f8d4b79807bbeaca5455ebbb142
James Batsell <jamesbatsell05@gmail.com>
2020-02-16 19:05:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,16 +68,11 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-  
- def all_holidays_with_bbq(holiday_hash)
  holiday_hash.collect do |season, holiday|
     holiday.collect do |holiday, item|
       holiday if item.include?("BBQ")
     end 
-  end
-end
+  end.flatten.compact
 end
 
 
